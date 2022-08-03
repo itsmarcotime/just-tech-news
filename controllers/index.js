8d04d7b1dd2e9224ffc039868b2664d56e59439e
Marco Mata <patrickstar1281@yahoo.com>
2022-08-03 03:36:01 -0500

    finished the homepage using handlebars

--- controllers/index.js
+++ controllers/index.js
@@ -1,13 +1,13 @@
 const router = require('express').Router();
 const apiRoutes = require('./api');
-const homeRoutes = require('./home-routes')
+const homeRoutes = require('./home-routes.js');
 
 router.use('/api', apiRoutes);
+router.use('/', homeRoutes);
 
 router.use((req, res) => {
   res.status(404).end();
 });
 
-router.use('/', homeRoutes);
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const routes = require('./controllers/');
+const routes = require('./controllers');
 const sequelize = require('./config/connection');
 const path = require('path');
 const exphbs = require('express-handlebars');
@@ -10,9 +10,10 @@ const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
 // turn on routes
 app.use(routes);
-app.use(express.static(path.join(__dirname, 'public')));
+
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
